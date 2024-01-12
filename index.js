c4d6b2b33eda73c426c7be2b0572734a69f5bf53
Tran Duc Khai / チャン.ドゥック.カイ <89479879+duckhai2323@users.noreply.github.com>
2024-01-12 14:58:37 +0700

    fix json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 
-app.get("/house", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     let allData = [];
 
